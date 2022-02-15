07a1be9c46c7bfcaef4fd9d1886bbd049c0c3b8a
Kenneth Gilmore <gilmorekenneth@gmail.com>
2022-02-15 16:05:41 -0500

    Added logic for verifying answers

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const gameboard = [
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-];
+import words from "./data/words.js";
 
 const keyboardButtons = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
@@ -15,36 +9,85 @@ const keyboardButtons = [
 const gameContainer = document.querySelector("#game-container");
 const keyboardContainer = document.querySelector("#keyboard-container");
 
+const wordCount = words.length;
+
+// Game state
+let answer = "";
 let guess = "";
 let guessCount = 1;
-guessHistory = [];
+let guessHistory = [];
 
+// Cycles through guess to reveal hints on gameboard and keyboard
+const revealHints = (currentGuess) => {
+  // Remove me
+  console.log(answer.join());
+
+  let guessArray = currentGuess.split("");
+  let answerCopy = [...answer];
+
+  // Cycles through to find exact matches
+  guessArray.forEach((letter, i) => {
+    const tile = document.querySelector(`#guess-${guessCount}-tile-${i + 1}`);
+    const keyboardKey = document.querySelector(`#${letter}`);
+    if (letter === answer[i]) {
+      answerCopy.splice(i, 1);
+      tile.classList.add("match");
+      keyboardKey.classList.add("match");
+    }
+  });
+
+  // Cycles through the rest to find partials and show wrong guesses
+  guessArray.forEach((letter, i) => {
+    const tile = document.querySelector(`#guess-${guessCount}-tile-${i + 1}`);
+    const keyboardKey = document.querySelector(`#${letter}`);
+    if (tile.classList.contains("match")) return;
+    if (answerCopy.some((l) => l === letter)) {
+      const index = answerCopy.findIndex((l) => l === letter);
+      answerCopy.splice(index, 1);
+      tile.classList.add("partial");
+      if (!keyboardKey.classList.contains("match")) {
+        keyboardKey.classList.add("partial");
+      }
+    } else {
+      if (!keyboardKey.classList.contains("match")) {
+        keyboardKey.classList.add("no-match");
+      }
+    }
+  });
+};
+
+// Handles the click event for all letters
 const handleButtonClick = (e) => {
   const letter = e.target.getAttribute("key");
   if (guess.length < 5) {
-    tile = document.querySelector(
+    const tile = document.querySelector(
       `#guess-${guessCount}-tile-${guess.length + 1}`
     );
     tile.innerText = letter.toUpperCase();
     guess += letter;
   }
-  console.log(guess);
 };
 
+// Handles click of enter key
 const handleEnterClick = () => {
+  revealHints(guess);
   guessHistory.push(guess);
   guess = "";
   guessCount++;
 };
 
+// Handles click of delete
 const handleDeleteClick = () => {
   if (guess.length > 0) {
-    tile = document.querySelector(`#guess-${guessCount}-tile-${guess.length}`);
+    const tile = document.querySelector(
+      `#guess-${guessCount}-tile-${guess.length}`
+    );
     tile.innerText = "";
     guess = guess.slice(0, -1);
   }
 };
 
+// Creates game board squares
 const gameboardInit = () => {
   for (let y = 1; y < 7; y++) {
     const gameRow = document.createElement("div");
@@ -60,6 +103,7 @@ const gameboardInit = () => {
   }
 };
 
+// Creates the keyboard buttons
 const keyboardInit = () => {
   keyboardButtons.forEach((row) => {
     const keyboardRow = document.createElement("div");
@@ -83,5 +127,11 @@ const keyboardInit = () => {
   });
 };
 
+// Picks answer from pool
+const generateAnswer = () => {
+  answer = words[Math.floor(Math.random() * wordCount)].split("");
+};
+
 gameboardInit();
 keyboardInit();
+generateAnswer();
